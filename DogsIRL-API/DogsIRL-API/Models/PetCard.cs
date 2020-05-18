69519cd206f8923ce457fd7b84e80b90cc531ba8
andrewbc.dev@outlook.com <andrewbc.dev@outlook.com>
2020-05-18 11:57:39 -0700

    Initial scaffolding. Petcards service with crud, petcard model, petcards controller, pet cards manager, database schema, gitignore.

---
+++ DogsIRL-API/DogsIRL-API/Models/PetCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogsIRL_API.Models
+{
+    public class PetCard
+    {
+        public int ID;
+        public string Name;
+        public string ImageURL;
+        public string Sex;
+        public string Owner;
+        public int AgeYears;
+        public DateTime Birthday;
+        public DateTime DateCreated;
+        public DateTime DateCollected;
+        public sbyte GoodDog;
+        public sbyte Floofiness;
+        public sbyte Energy;
+        public sbyte Snuggles;
+        public sbyte Appetite;
+        public sbyte Bravery;
+    }
+}

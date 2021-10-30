60ca391c4435e3d6b80fff8163c172b2f872f7f3
andrewbc.dev@outlook.com <andrewbc.dev@outlook.com>
2021-10-30 00:37:32 -0700

    Added first pet card test, removed old account tests.

--- DogsIRL-API/Dogs-IRL-API-tests/PetCardTests.cs
+++ DogsIRL-API/Dogs-IRL-API-tests/PetCardTests.cs
@@ -7,20 +7,41 @@ using DogsIRL_API.Models;
 using DogsIRL_API.Models.Services;
 using DogsIRL_API.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Dogs_IRL_API_tests
 {
     public class PetCardTests
     {
         [Fact]
-        public void CanCreatePetCard()
+        public async Task CanCreatePetCard()
         {
             DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase("TestCanCreatePetCard")
                 .Options;
             using (ApplicationDbContext context = new ApplicationDbContext(options))
             {
-                var TestPetCardsService = new PetCardsService(context);
+                var testPetCardsService = new PetCardsService(context);
+                var testPetCard = new PetCard()
+                {
+                    Name = "TestDog",
+                    ImageURL = String.Empty,
+                    Sex = "Female",
+                    Owner = "Test",
+                    AgeYears = 0,
+                    Birthday = DateTime.Now,
+                    DateCreated = DateTime.Now,
+                    DateCollected = DateTime.Now,
+                    GoodDog = 1,
+                    Floofiness = 1,
+                    Energy = 1,
+                    Snuggles = 1,
+                    Appetite = 1,
+                    Bravery = 1,
+                    Collections = 1
+                };
+                var result = await testPetCardsService.CreatePetCard(testPetCard);
+                Assert.Equal(testPetCard, result);
             }
                 
         }
